2a4c037cb105a2966ad47c228312666dccb2d525
mjgoerlitz-new <mjgoerlitz@icloud.com>
2024-01-08 22:30:03 -0800

    more work on main file

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 )
 
 type Address struct {
@@ -20,5 +21,29 @@ type User struct {
 }
 
 func main() {
+	dir := "./"
 
+	db, err := New(dir, nil)
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+
+	employees := []User{
+		{"John", "23", "1234567", "Google", Address{"San Francisco", "California", "United States", "94105"}},
+		{"Emma", "29", "8743210", "Amazon", Address{"Seattle", "Washington", "United States", "98109"}},
+		{"Carlos", "35", "7625894", "Microsoft", Address{"Redmond", "Washington", "United States", "98052"}},
+		{"Aisha", "31", "9456732", "Apple", Address{"Cupertino", "California", "United States", "95014"}},
+		{"Liam", "28", "6182947", "Facebook", Address{"Menlo Park", "California", "United States", "94025"}},
+		{"Yuna", "26", "5371826", "Netflix", Address{"Los Gatos", "California", "United States", "95032"}},
+	}
+
+	for _, value := range employees {
+		db.Write("users", value.Name, User{
+			Name:    value.Name,
+			Age:     value.Age,
+			Contact: value.Contact,
+			Company: value.Company,
+			Address: value.Address,
+		})
+	}
 }
